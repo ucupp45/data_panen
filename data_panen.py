4ba603c5967a723465d78ba2a966fe105a245be2
Yusup Kurniawan <yusupkurniawan147@gmail.com>
2024-12-05 14:09:03 +0700

    ini versi baru

--- data_panen.py
+++ data_panen.py
@@ -103,3 +103,5 @@ for lokasi, data in data_panen.items():
         print(f"Lokasi {nama_lokasi} memerlukan perhatian khusus.")
     else:
         print(f"Lokasi {nama_lokasi} dalam kondisi baik.")
+        
+print ("Melakukan Perubahan")
